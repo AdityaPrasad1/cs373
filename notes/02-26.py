49485afb127ee2e4e8979cde65ae08163608a4a3
Glenn Downing <downing@cs.utexas.edu>
2018-02-26 16:59:52 -0600

    another commit

--- notes/02-26.py
+++ notes/02-26.py
@@ -2,3 +2,22 @@
 # Mon, 26 Feb
 # -----------
 
+"""
+1. build map as a class
+2. build map as a function with yield
+3. build map as a function, one line, no yield
+"""
+
+"""
+classic example of map (transform) / reduce (accumulate) is something like the sum of the squares
+
+other good examples are standard deviation and root-mean-square error (look those up!)
+"""
+
+"""
+map / reduce is at the heart of Google's search engine:
+
+https://static.googleusercontent.com/media/research.google.com/en//archive/mapreduce-osdi04.pdf
+
+and that technology has now evolved into Hadoop
+"""
