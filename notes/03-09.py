030e66dc3eaeb8757e9aa15cb6ae83a185675235
Glenn Downing <downing@cs.utexas.edu>
2018-03-09 17:19:48 -0600

    another commit

--- notes/03-09.py
+++ notes/03-09.py
@@ -2,3 +2,19 @@
 # Fri,  9 Mar
 # -----------
 
+"""
+three tokens: ==, *, **
+two contexts: a function call, a function definition
+"""
+
+print(2)
+print(2, end=" ") # end must be provided by name
+
+"""
+* unpacking and by position have the same precedence, and whoever goes first wins
+* unpacking and by name do NOT have the same precedence, unpacking wins
+* unpacking needs an iterable
+
+** unpacking needs a dict or something like dict
+** unpacking and by name have the same precedence, but must have no conflict
+"""
