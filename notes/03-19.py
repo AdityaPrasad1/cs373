2e418bb4fc4a1c51e975adddf3172d2499fb393d
Glenn Downing <downing@cs.utexas.edu>
2018-03-19 20:08:30 -0500

    another commit

--- notes/03-19.py
+++ notes/03-19.py
@@ -2,3 +2,61 @@
 # Mon, 19 Mar
 # -----------
 
+"""
+relational algebra
+
+algebras
+    set of elements
+    set of operations
+    open/closed on those operations
+
+integer arithmetic
+    integers
+    +, -, *, /, %
+    on +: closed
+    on *: closed
+    on -: closed
+    on %: closed
+    on /: open
+
+relational algebra
+    relations (tables)
+    select, project, many flavors of join
+    always closed
+
+movie table
+
+title year director genre
+"shane" 1953 "george stevens" "western"
+"star wars" 1977 "george lucas" "western"
+
+director table
+
+id name
+1 "george stevens"
+2 "george lucas"
+
+movie table
+
+title year id genre
+"shane" 1953 1 "western"
+"star wars" 1977 2 "western"
+
+SQL is a DECLARATIVE language
+compare that to a PROCEDURAL language (Pyton, C, JavaScript, ...)
+
+relational algebra
+    select
+        input
+            table
+            a predicate
+        output
+            table
+
+    project
+        input
+            table
+            set of attributes (col names)
+        output
+            table
+"""
