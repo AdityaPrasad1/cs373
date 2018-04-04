2d4d195d2d839681fcb3a858634d2ac2e1d5bb66
Glenn Downing <downing@cs.utexas.edu>
2018-04-04 15:46:30 -0500

    another commit

--- notes/04-02.py
+++ notes/04-02.py
@@ -2,3 +2,22 @@
 # Mon,  2 Apr
 # -----------
 
+"""
+relational algebra
+    select  table, unary predicate
+    project table, attributes...
+
+    join
+        cross join table1, table2
+            let's pretend that there are 10 students and 15 applications
+            150 rows in result
+
+        theta join table1, table2, predicate
+            SQL's inner join with either ON or USING
+            ON    allows different  field names
+            USING requires the same filed name
+
+        natural join table1, table2
+
+        self join table
+"""
---
+++ notes/04-04.py
@@ -0,0 +1,18 @@
+# -----------
+# Wed,  4 Apr
+# -----------
+
+"""
+filtering, pagination, searching, sorting
+
+be active as assessor, use the site yourself, instead of just being shown
+try several tests, does anything not work?
+
+ask lots of questions: how did they implement it?
+
+what did they do better than your group?
+
+what did your group do better than them?
+
+discuss those differences
+"""
