9ffb05870495463d4df091b381e6a673509ab14e
Glenn Downing <downing@cs.utexas.edu>
2018-04-13 16:59:45 -0500

    another commit

---
+++ notes/04-13.py
@@ -0,0 +1,29 @@
+# -----------
+# Fri, 13 Apr
+# -----------
+
+"""
+Replace Type Code with Subclasses
+Replace Type Code with State / Strategy
+
+subtype / subclass movie
+    1. have to change to change the tests
+    2. can't change type of movie easily
+
+there are two relationships between classes
+    1. inheritance
+        a. reuse implementation
+        b. reuse interface
+        c. can't change your parents
+    2. containment
+        a. reuse implementation
+        b. don't get interface
+        c. no problem in changing what you contain
+
+Java
+    class Vector
+    class Stack extends Vector
+C++
+    class vector
+    class stack contains vector
+"""
