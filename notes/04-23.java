cbdd7f28829e218eb2e091c1d5288e8888ba33ff
Glenn Downing <downing@cs.utexas.edu>
2018-04-23 16:41:53 -0500

    another commit

--- notes/04-23.java
+++ notes/04-23.java
@@ -2,3 +2,25 @@
 // Mon, 23 Apr
 // -----------
 
+abstract class A {
+    abstract void f (int);}
+
+class B extends A {
+    void f (int) {...}};
+
+class T {
+    public static void main (...) {
+        A x = new B;
+        x.f(2);      // B.f
+
+/*
+three consequences to an abstract method
+    1. must mark the class abstract
+    2. children must define the method
+    3. can't define the method in the parent
+*/
+
+class A {
+    void f (int) {...}
+    final void g (int) {...}
+    abstract void h (int);}
